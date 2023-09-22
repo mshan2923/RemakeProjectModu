c9e8d2db05ac8153210c78967d0818536dde2448
mshan2923 <mshan2923@gmail.com>
2023-09-22 15:50:19 +0900

    Add CORS - Access All

--- modu/src/main/java/com/example/modu/ModuApplication.java
+++ modu/src/main/java/com/example/modu/ModuApplication.java
@@ -3,6 +3,9 @@ package com.example.modu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 		//(exclude = SecurityAutoConfiguration.class)//Security 비활성화
@@ -10,6 +13,16 @@ public class ModuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModuApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");//http://front-server.com
+			}
+		};
 	}
 
 }
